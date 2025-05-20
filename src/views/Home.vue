555bae9f9deaffb1932083b1642ad9a6eec641e1
KriawqZero <supergamerbr39@gmail.com>
2025-05-20 18:23:15 -0400

    .

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,9 @@
 <template>
   <div>
-    <h1 class="bg-dark text-white"> Home </h1>
-    <h2>Count: {{ count }}</h2>
     <CountButton />
   </div>
 </template>
 
 <script setup lang="ts">
-  import { ref } from 'vue'
   import CountButton from '@/components/CountButton.vue'
-
-  const count = ref(0)
 </script>
